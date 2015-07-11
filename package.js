6ebe6392a0fcfaad5f58878b8905e99795450a8c
Pauli Jokela <pauli.jokela@didstopia.com>
2015-07-11 12:17:58 +0300

    Trying to fix the package

--- package.js
+++ package.js
@@ -2,9 +2,13 @@ Package.describe(
 {
   name: 'didstopia:supportkit',
   summary: 'Provides easy access to SupportKit.',
-  version: '0.0.4',
+  version: '0.0.5',
   git: 'https://github.com/Didstopia/meteor-supportkit.git',
-  documentation: 'README.md'
+  documentation: 'README.md',
+  packages:
+  {
+    
+  }
 });
 
 Npm.depends(
@@ -15,13 +19,18 @@ Npm.depends(
 Package.onUse(function(api)
 {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('supportkit.js', 'client');
+
+  api.addFiles('meteor-supportkit.js', 'client');
+
   api.export('SupportKit', 'client');
 });
 
 Package.onTest(function(api)
 {
   api.use(['tinytest', 'didstopia:supportkit']);
-  api.addFiles('supportkit-tests.js', 'client');
+
+  api.addFiles('meteor-supportkit.js', 'client');
+  api.addFiles('meteor-supportkit-tests.js', 'client');
+
   api.export('SupporKit', 'client');
 });
