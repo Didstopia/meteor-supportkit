633d5b89c3a521be58f0556341e4bba0db9274f9
Pauli Jokela <pauli.jokela@didstopia.com>
2015-07-11 10:33:01 +0300

    Updated package.js version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe(
 {
   name: 'didstopia:supportkit',
   summary: 'Provides easy access to SupportKit.',
-  version: '0.0.2',
+  version: '0.0.3',
   git: 'https://github.com/Didstopia/meteor-supportkit.git',
   documentation: 'README.md'
 });
